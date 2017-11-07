9198a52fe4dbbd500c807115784fdd0ca84d3263
Mark Lawrenson <mark@lawrenson.id.au>
2017-11-07 14:10:16 +1100

    Working on it

--- libmf/mf.py
+++ libmf/mf.py
@@ -2,9 +2,16 @@ import numpy as np
 import ctypes
 import os
 import sys
+import re
 
-compiled_src = os.environ["LIBMF_OBJ"] if "LIBMF_OBJ" in os.environ else sys.argv[1] if len(sys.argv) > 1 else \
-    r'C:\Users\mark\repos\python-libmf\build\lib.win-amd64-3.6\libmf.cp36-win_amd64.pyd'
+def find_lib_path():
+    path = os.environ["LIBMF_OBJ"] if "LIBMF_OBJ" in os.environ else sys.argv[1] if len(sys.argv) > 1 else \
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    for i in os.listdir(path):
+        if os.path.isfile(os.path.join(path, i)) and re.match('python-libmf\.cpython(.*)\.so', i):
+            return os.path.join(path, i)
+
+compiled_src = find_lib_path()
 mf = ctypes.CDLL(compiled_src)
 c_float_p = ctypes.POINTER(ctypes.c_float)
 
